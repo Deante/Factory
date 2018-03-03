e2d668d1b42ec58e0a3472df53fd7a11636dfd82
thomas <tgrappin1986@gmail.com>
2018-03-03 21:05:56 +0100

    ajout des autorités manquantes

--- src/main/java/factory/security/AuthoritiesConstants.java
+++ src/main/java/factory/security/AuthoritiesConstants.java
@@ -7,6 +7,14 @@ public final class AuthoritiesConstants {
 
     public static final String ADMIN = "ROLE_ADMIN";
 
+    public static final String GESTIONNAIRE = "ROLE_GESTIONNAIRE";
+
+    public static final String FORMATEUR = "ROLE_FORMATEUR";
+
+    public static final String STAGIAIRE = "ROLE_STAGIAIRE";
+
+    public static final String TECHNICIEN = "ROLE_TECHNICIEN";
+
     public static final String USER = "ROLE_USER";
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
