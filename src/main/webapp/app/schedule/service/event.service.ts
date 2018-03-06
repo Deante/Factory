a156e820906909adae65461893959acaf9326b53
julien bertrand <julien.b.bertrand@outlook.com>
2018-03-06 16:54:38 +0100

    get entity object from schedule component

--- src/main/webapp/app/schedule/service/event.service.ts
+++ src/main/webapp/app/schedule/service/event.service.ts
@@ -1,7 +1,9 @@
-import {Injectable} from '@angular/core';
+import { Injectable, LOCALE_ID, NgModule } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import { Formation, FormationService } from '../../entities/formation';
+import { ResponseWrapper } from '../../shared';
 
 @Injectable()
 export class EventService {
@@ -12,5 +14,9 @@ export class EventService {
     getEvents(): Observable<any> {
         return this.http.get('content/primeng/assets/data/json/events/scheduleevents.json')
             .map((response) => response);
+    }
+
+    getFormationEvents(formationService: FormationService): Observable<any> {
+        return formationService.query();
     }
 }
