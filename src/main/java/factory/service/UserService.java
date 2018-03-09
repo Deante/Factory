00d67ff79220319c66df658f736a1810eaf4c238
sonny <sonnyson95@gmail.com>
2018-03-09 10:44:42 +0100

    password set

--- src/main/java/factory/service/UserService.java
+++ src/main/java/factory/service/UserService.java
@@ -142,7 +142,7 @@ public class UserService {
                 .collect(Collectors.toSet());
             user.setAuthorities(authorities);
         }
-        String encryptedPassword = passwordEncoder.encode(RandomUtil.generatePassword());
+        String encryptedPassword = passwordEncoder.encode("password");
         user.setPassword(encryptedPassword);
         user.setResetKey(RandomUtil.generateResetKey());
         user.setResetDate(Instant.now());
